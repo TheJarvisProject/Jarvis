9bb7f6e5461d54398e86f6424414a1ee39ca8225
HittmanA <hittmana@comcast.net>
2017-08-05 01:56:00 -0400

    Added config stuff and started on youtube module.

---
+++ plugins/youtubePlay.js
@@ -0,0 +1,31 @@
+module.exports = {
+    requirements: "[youtube, play] {intent}",
+    name: "YoutubePlay",
+    version: "0.1.0",
+    OnLoad: function() {
+        this.Info(this.name + " " + this.version + " loaded!");
+        this.createConfig();
+        this.addConfig("plugin", {key: "YOURKEY"});
+    },
+
+    run: function(input, request) {
+        var search = require('youtube-search');
+        
+        var opts = {
+          maxResults: 10,
+          key: this.getConfigValue("key")
+        };
+
+        var val = input._text;
+        val = val.replace(/play/i,"");
+        val = val.replace(/youtube/i,"");
+
+        search(val, opts, function(err, results) {
+          if(err) return console.log(err);
+
+          console.dir(results);
+        });
+
+        return val;
+    }
+}
