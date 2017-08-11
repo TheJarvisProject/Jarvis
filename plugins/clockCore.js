71e232e0a5d01469d4f9f91adecb45705acec2bc
herohamp <hampmoe@me.com>
2017-08-11 18:47:52 -0400

    Alarm at date. Supports wildcards

--- plugins/clockCore.js
+++ plugins/clockCore.js
@@ -19,13 +19,57 @@ module.exports = {
     const TTS = require("../core/TTS.js");
     const Logger = require("../core/Logger.js");
     const logger = new Logger(this.name);
+    const date = new Date();
+    // Time format is Year:Month:Weekday:Day:Hour:Minute:Second
 
+    for (i in this.data) {
+      const timeSplit = this.data[i].time.split(":")
+      let check = [false, false, false, false, false, false, false];
 
+      if (timeSplit[0] == "*") {
+        check[0] = true
+      } else if (timeSplit[0] == date.getFullYear()) {
+        check[0] = true
+      }
 
-    for (i in this.data){
-      if (this.data[i].time ==  Math.round((new Date()).getTime() / 1000).toString()){
-        logger.Info("Tick " + this.data[i].message)
-        TTS(this.data[i].message);
+      if (timeSplit[1] == "*") {
+        check[1] = true
+      } else if (timeSplit[1] == date.getMonth() + 1) {
+        check[1] = true
+      }
+
+      if (timeSplit[2] == "*") {
+        check[2] = true
+      } else if (timeSplit[2] == date.getDay() + 1) {
+        check[2] = true
+      }
+
+      if (timeSplit[3] == "*") {
+        check[3] = true
+      } else if (timeSplit[3] == date.getDate()) {
+        check[3] = true
+      }
+
+      if (timeSplit[4] == "*") {
+        check[4] = true
+      } else if (timeSplit[4] == date.getHours()) {
+        check[4] = true
+      }
+
+      if (timeSplit[5] == "*") {
+        check[5] = true
+      } else if (timeSplit[5] == date.getMinutes()) {
+        check[5] = true
+      }
+
+      if (timeSplit[6] == "*") {
+        check[6] = true
+      } else if (timeSplit[6] == date.getSeconds()) {
+        check[6] = true
+      }
+
+      if (check[0]==true && check[1]==true && check[2]==true && check[3]==true && check[4]==true && check[5]==true && check[6]==true){
+        TTS(this.data[i].message)
       }
 
     }
